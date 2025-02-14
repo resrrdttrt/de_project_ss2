795ef80d27d3a18ddd6a5a36591cf53d403d3908
resrrdttrt <resrrdttrt@gmail.com>
2025-02-14 10:15:31 +0000

    update local

--- command.sh
+++ command.sh
@@ -3,5 +3,7 @@ sudo apt update
 sudo apt install docker.io -y
 sudo apt install docker-compose -y
 sudo apt install net-tools -y
+sudo apt install python3.10-venv -y
+sudo apt install python3-pip -y
 
 sudo chmod -R 777 /var/run/docker.sock
